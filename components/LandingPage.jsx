ab40dbbd0a58f587c91d57dd62ead8cb68d7f9c7
Sophie Woodcock <szoph101@hotmail.co.uk>
2024-01-19 11:42:57 +0000

    final

--- components/LandingPage.jsx
+++ components/LandingPage.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const LandingPage = () => {
   return (
-    <div className=''>
-        <div className='p-5 m-5 px-60 mt-40 text-white bg-gradient'>
+    <div className='bg-gradient absolute h-full w-full'>
+        <div className='p-5 m-5 px-60 mt-40 text-white flex justify-center items-center flex-col'>
             <h1 className='font-thin text-3xl pb-9'>Welcome to Weather App!</h1>
             <p className='font-thin text-xl'>Enter your city in the search bar above!</p>
         </div>
