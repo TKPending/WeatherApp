1dc59ca9a5f487bb06f77be00c6149d477e6f13b
Sophie Woodcock <szoph101@hotmail.co.uk>
2024-01-19 12:04:21 +0000

    api key

--- app/api/geocoding/route.js
+++ app/api/geocoding/route.js
@@ -2,13 +2,13 @@ import axios from "axios";
 import { NextResponse } from "next/server";
 
 export async function GET(userInput) {
-    const APIKEY = process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY;
+    const APIKEY = process.env.OPENWEATHER_API_KEY;
     try {
         const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${userInput}&limit=5&appid=${APIKEY}`);
         const data = response.data[0] ? response.data[0] : null;
         
         if (!data) {
-            return undefined;
+            return NextResponse.error();
         }
 
         const lat = data.lat;
